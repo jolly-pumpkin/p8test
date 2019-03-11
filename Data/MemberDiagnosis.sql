43f2a0c483e35fe01c42f13c9f1efb0bc25ba2ef
Collin <cneill.m@gmail.com>
2019-03-10 22:13:29 -0400

    sql fun

--- Data/MemberDiagnosis.sql
+++ Data/MemberDiagnosis.sql
@@ -57,7 +57,7 @@ LEFT JOIN DiagnosisCategory dc ON dcm.DiagnosisCategoryID = dc.DiagnosisCategory
 Group BY m.MemberID
 order by m.MemberID
 
-
+SELECT MemberID, FirstName, LastName FROM Member WHERE ID = 
 
 
 
@@ -71,27 +71,96 @@ order by m.MemberID
 
 select 
 M.MemberID, M.FirstName, M.LastName,
-sd.MostSevereDiagnosisID 
+sd.MostSevereDiagnosisID, sd.DiagnosisDescription,
+dc.DiagnosisCategoryID as 'CategoryID', dc.CategoryDescription, dc.CategoryScore
+
 from Member m
 LEFT JOIN (
 	select  
-	m.MemberID,dc.DiagnosisCategoryID, MIN(d.DiagnosisID )as 'MostSevereDiagnosisID'
+	m.MemberID,dc.DiagnosisCategoryID, MIN(d.DiagnosisID )as 'MostSevereDiagnosisID', d.DiagnosisDescription
 	from Member m
 	LEFT JOIN MemberDiagnosis md on md.MemberID = m.MemberID
 	LEFT JOIN Diagnosis d ON d.DiagnosisID = md.DiagnosisID
 	LEFT JOIN DiagnosisCategoryMap dcm ON d.DiagnosisID = dcm.DiagnosisID
 	LEFT JOIN DiagnosisCategory dc ON dcm.DiagnosisCategoryID = dc.DiagnosisCategoryID
-	Group BY m.MemberID, dc.DiagnosisCategoryID
+	Group BY m.MemberID, dc.DiagnosisCategoryID, d.DiagnosisDescription
 	) sd
 ON sd.MemberID = m.MemberID
-
-
-select  
-m.MemberID,min(dc.DiagnosisCategoryID) as 'MostSevereCategoryId'
-from Member m
-LEFT JOIN MemberDiagnosis md on md.MemberID = m.MemberID
-LEFT JOIN Diagnosis d ON d.DiagnosisID = md.DiagnosisID
-LEFT JOIN DiagnosisCategoryMap dcm ON d.DiagnosisID = dcm.DiagnosisID
+LEFT JOIN DiagnosisCategoryMap dcm ON sd.MostSevereDiagnosisID = dcm.DiagnosisID
 LEFT JOIN DiagnosisCategory dc ON dcm.DiagnosisCategoryID = dc.DiagnosisCategoryID
-Group BY m.MemberID
-order by m.MemberID
+
+LEFT JOIN (
+	select  
+	m.MemberID,min(dc.DiagnosisCategoryID) as 'MostSevereCategoryId', 1 as 'Is Most Server Category'
+	from Member m
+	LEFT JOIN MemberDiagnosis md on md.MemberID = m.MemberID
+	LEFT JOIN Diagnosis d ON d.DiagnosisID = md.DiagnosisID
+	LEFT JOIN DiagnosisCategoryMap dcm ON d.DiagnosisID = dcm.DiagnosisID
+	LEFT JOIN DiagnosisCategory dc ON dcm.DiagnosisCategoryID = dc.DiagnosisCategoryID
+	Group BY m.MemberID
+	)sc
+ON sc.MostSevereCategoryId = dc.DiagnosisCategoryID
+
+
+
+--distinct
+
+
+SELECT 
+*
+--m.MemberID, m.FirstName, m.LastName
+FROM Member m
+INNER JOIN (
+	select 
+		distinct
+		m.MemberID,
+		dm.DiagnosisCategoryID
+	from Member m
+	LEFT JOIN MemberDiagnosis md ON md.MemberID = m.MemberID
+	LEFT JOIN DiagnosisCategoryMap dm ON dm.DiagnosisID = md.DiagnosisID
+	LEFT JOIN DiagnosisCategory dc ON dc.DiagnosisCategoryID = dm.DiagnosisCategoryID
+	)dd
+ON dd.MemberID = m.MemberID
+
+select 
+*
+from Member m
+ JOIN 
+	(
+		select 
+		m.MemberID, MIN(md.DiagnosisID) As 'MostSevereDiagnosis',  dm.DiagnosisCategoryID
+		from Member m
+		LEFT JOIN MemberDiagnosis md ON md.MemberID = m.MemberID
+		LEFT JOIN DiagnosisCategoryMap dm ON dm.DiagnosisID = md.DiagnosisID
+		GROUP BY m.MemberID, dm.DiagnosisCategoryID
+	)msd
+ON msd.MemberID = m.MemberID
+
+
+
+	select 
+		distinct
+		m.MemberID,
+		dm.DiagnosisCategoryID,
+		msd.MostSevereDiagnosis
+	from Member m
+	LEFT JOIN MemberDiagnosis md ON md.MemberID = m.MemberID
+	LEFT JOIN DiagnosisCategoryMap dm ON dm.DiagnosisID = md.DiagnosisID
+	LEFT JOIN DiagnosisCategory dc ON dc.DiagnosisCategoryID = dm.DiagnosisCategoryID
+left JOIN(
+	select 
+		m.MemberID, MIN(md.DiagnosisID) As 'MostSevereDiagnosis',  dm.DiagnosisCategoryID
+		from Member m
+		LEFT JOIN MemberDiagnosis md ON md.MemberID = m.MemberID
+		LEFT JOIN DiagnosisCategoryMap dm ON dm.DiagnosisID = md.DiagnosisID
+		GROUP BY m.MemberID, dm.DiagnosisCategoryID
+)msd ON msd.MemberID = m.MemberID
+
+
+	select 
+		m.MemberID, MIN(md.DiagnosisID) As 'MostSevereDiagnosis',  dm.DiagnosisCategoryID
+		
+		from Member m
+		LEFT JOIN MemberDiagnosis md ON md.MemberID = m.MemberID
+		LEFT JOIN DiagnosisCategoryMap dm ON dm.DiagnosisID = md.DiagnosisID
+		GROUP BY m.MemberID, dm.DiagnosisCategoryID, md.DiagnosisID
